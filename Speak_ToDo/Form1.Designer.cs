82f0346a7833c2154fef4cb999537e272292a6da
sakuzyokun <sakuzyokun.sakuzyo0606@gmail.com>
2025-07-30 20:27:48 +0900

    Add files via upload

---
+++ Speak_ToDo/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TalkingToDo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxTask = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonSpeak = new System.Windows.Forms.Button();
+            this.listViewTasks = new System.Windows.Forms.ListView();
+            this.check = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.task = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxTask
+            // 
+            this.textBoxTask.Location = new System.Drawing.Point(12, 12);
+            this.textBoxTask.Name = "textBoxTask";
+            this.textBoxTask.Size = new System.Drawing.Size(418, 19);
+            this.textBoxTask.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(355, 37);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "追加";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(320, 161);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(110, 23);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "選択したものを削除";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonSpeak
+            // 
+            this.buttonSpeak.Location = new System.Drawing.Point(12, 190);
+            this.buttonSpeak.Name = "buttonSpeak";
+            this.buttonSpeak.Size = new System.Drawing.Size(97, 23);
+            this.buttonSpeak.TabIndex = 4;
+            this.buttonSpeak.Text = "タスクの読み上げ";
+            this.buttonSpeak.UseVisualStyleBackColor = true;
+            this.buttonSpeak.Click += new System.EventHandler(this.buttonSpeak_Click);
+            // 
+            // listViewTasks
+            // 
+            this.listViewTasks.CheckBoxes = true;
+            this.listViewTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.check,
+            this.task,
+            this.date});
+            this.listViewTasks.HideSelection = false;
+            this.listViewTasks.Location = new System.Drawing.Point(10, 67);
+            this.listViewTasks.Name = "listViewTasks";
+            this.listViewTasks.Size = new System.Drawing.Size(420, 88);
+            this.listViewTasks.TabIndex = 7;
+            this.listViewTasks.UseCompatibleStateImageBehavior = false;
+            this.listViewTasks.View = System.Windows.Forms.View.Details;
+            // 
+            // check
+            // 
+            this.check.Text = "完了";
+            this.check.Width = 50;
+            // 
+            // task
+            // 
+            this.task.Text = "タスク";
+            this.task.Width = 200;
+            // 
+            // date
+            // 
+            this.date.Text = "追加日時";
+            this.date.Width = 150;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 158);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(123, 12);
+            this.labelStatus.TabIndex = 8;
+            this.labelStatus.Text = "完了: 0 件 / 全体: 0 件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 222);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.listViewTasks);
+            this.Controls.Add(this.buttonSpeak);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxTask);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TalkingToDo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTask;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonSpeak;
+        private System.Windows.Forms.ListView listViewTasks;
+        private System.Windows.Forms.ColumnHeader task;
+        private System.Windows.Forms.ColumnHeader date;
+        private System.Windows.Forms.ColumnHeader check;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ Speak_ToDo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Linq;
+
+namespace TalkingToDo
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            synth.SetOutputToDefaultAudioDevice();
+
+            // ListView 設定
+            listViewTasks.View = View.Details;
+            listViewTasks.CheckBoxes = true;
+            /*listViewTasks.Columns.Add("完了", 50);
+            listViewTasks.Columns.Add("タスク", 200);
+            listViewTasks.Columns.Add("追加日時", 150);*/
+            listViewTasks.FullRowSelect = true;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxTask.Text))
+            {
+                string taskText = textBoxTask.Text.Trim();
+                string now = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+
+                ListViewItem item = new ListViewItem(); // チェックボックス
+                item.SubItems.Add(taskText);            // タスク
+                item.SubItems.Add(now);                 // 日付
+
+                listViewTasks.Items.Add(item);
+                textBoxTask.Clear();
+                textBoxTask.Focus();
+
+                UpdateCompletedCount();
+                UpdateStatusLabel();
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listViewTasks.SelectedItems)
+            {
+                listViewTasks.Items.Remove(item);
+            }
+            UpdateStatusLabel();
+        }
+
+        private void buttonSpeak_Click(object sender, EventArgs e)
+        {
+            if (listViewTasks.SelectedItems.Count > 0)
+            {
+                var item = listViewTasks.SelectedItems[0];
+                string taskText = item.SubItems[1].Text;
+
+                synth.SpeakAsync(taskText);
+            }
+        }
+
+        private void listViewTasks_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            foreach (ListViewItem item in listViewTasks.Items)
+            {
+                if (item.Checked)
+                {
+                    item.ForeColor = System.Drawing.Color.Gray;
+                }
+                else
+                {
+                    item.ForeColor = System.Drawing.Color.Black;
+                }
+            }
+
+            UpdateCompletedCount(); // ↓この関数でカウント表示も更新！
+            UpdateStatusLabel();
+        }
+
+        private void UpdateCompletedCount()
+        {
+            int completed = 0;
+            foreach (ListViewItem item in listViewTasks.Items)
+            {
+                if (item.Checked)
+                    completed++;
+            }
+
+            labelStatus.Text = $"完了: {completed} 件 / 全体: {listViewTasks.Items.Count} 件";
+        }
+
+        private void UpdateStatusLabel()
+        {
+            int total = listViewTasks.Items.Count;
+            int completed = listViewTasks.Items.Cast<ListViewItem>().Count(item => item.Checked);
+            labelStatus.Text = $"完了: {completed} 件 / 全体: {total} 件";
+        }
+    }
+}
